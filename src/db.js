615a382698eafe0e54c6eddd4c9f4c6878c33748
Ahmed Shakeel <72567856+Ahmed-Shakeel-798@users.noreply.github.com>
2023-01-30 01:23:15 +0500

    updated db.js

--- src/db.js
+++ src/db.js
@@ -4,7 +4,6 @@ const uri1 = process.env.MONGODB_URL1;
 const uri2 = process.env.MONGODB_URL2;
 
 const connection1 = mongoose.createConnection(uri1);
-
 const connection2 = mongoose.createConnection(uri2);
 
 module.exports = {
